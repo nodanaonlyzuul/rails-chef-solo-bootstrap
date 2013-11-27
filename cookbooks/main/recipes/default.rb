258959a1398b108756daa3c16f30db627095b064
Stephen Schor <stephen@eastmedia.com>
2013-11-27 09:43:50 -0500

    Add mysql cookbook and install the redis-server package

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,6 +1,7 @@
 # This should live on your server here: /var/chef/cookbooks/main/recipes/default.rb
 
 package "git-core"
+package "redis-server"
 include_recipe "imagemagick::rmagick"
 
 user node[:user][:name] do
