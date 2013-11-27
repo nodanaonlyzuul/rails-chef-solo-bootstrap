cbfe2511b1aaa5a6fb224786b4a4ac4759a0088b
Stephen Schor <stephen@eastmedia.com>
2013-11-27 02:21:32 -0500

    recipe[main] will create a deploy user

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,3 +1,10 @@
 # This should live on your server here: /var/chef/cookbooks/main/recipes/default.rb
 
 package "git-core"
+
+user node[:user][:name] do
+  password node[:user][:password]
+  gid "admin"
+  home "/home/#{node[:user][:name]}"
+  supports manage_home: true
+end
